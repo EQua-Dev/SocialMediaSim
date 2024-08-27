1e841733584b5333406774a8cc69afc52ce3316c
EQua-Dev <equality560@gmail.com>
2024-08-27 15:54:52 +0100

    designed login ui

---
+++ SocialMediaSim/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  SocialMediaSim
+//
+//  Created by Richard Uzor on 27/08/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
