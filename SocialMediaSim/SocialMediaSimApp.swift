10540cc3cb1af1c13ea2835bae6820543713adea
EQua-Dev <equality560@gmail.com>
2024-08-27 11:46:50 +0100

    Initial Commit

---
+++ SocialMediaSim/SocialMediaSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaSimApp.swift
+//  SocialMediaSim
+//
+//  Created by Richard Uzor on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
